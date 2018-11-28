f47a3a1080b2d78587c50c546a7fe59cc1dcdd56
saulsierra18 <se.sierraluna@ugto.mx>
2018-11-28 09:26:46 -0600

    Actualizado

---
+++ proyecto.c
@@ -0,0 +1,8 @@
+//
+//  proyecto.c
+//  
+//
+//  Created by Saul Sierra Luna on 11/28/18.
+//
+
+#include "proyecto.h"
---
+++ proyecto.h
@@ -0,0 +1,13 @@
+//
+//  proyecto.h
+//  
+//
+//  Created by Saul Sierra Luna on 11/28/18.
+//
+
+#ifndef proyecto_h
+#define proyecto_h
+
+#include <stdio.h>
+
+#endif /* proyecto_h */
