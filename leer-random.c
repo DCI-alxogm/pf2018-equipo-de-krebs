88ea96b0abb37b893c6aaad9a81acae7711192e6
Godinez Brizuela Salma Patricia <godinezbs2017@dci25-160.ugto.mx>
2018-12-05 09:42:23 -0600

    Actualizado.

---
+++ leer-random.c
@@ -0,0 +1,81 @@
+//Este codigo lee los archivos random y guarda sus datos en arreglos
+//Creado por Patricia Godinez el 5 de diciembre de 2018
+#include<stdio.h>
+
+int main()
+{
+	FILE *fp;
+	FILE *fs;
+	FILE *fp2;
+	FILE *fs2;
+
+	int nol=0,nw=0,nc=0; //nol es el numero de lineas que tenemos en el archivo 
+	char ch;
+//contador de lineas del archivo para poder leer solo los datos necesarios del archivo
+    fs=fopen("random.txt","r");
+    while((ch=fgetc(fs))!=EOF)
+    {
+        if(ch==' ')
+            nw++;
+        else if(ch=='\n')
+            nol++;
+        else
+            nc++;
+        
+        
+    }
+//leyendo el archivo random.txt para guardar las coordenadas en arreglos.
+	float xd[255], yd[255], zd[255];
+	int i, con=0;
+	fp=fopen("random.txt","r");
+	for(i=0;i<nol;i++)
+	{
+		fscanf(fp,"%f	%f	%f",&xd[i],&yd[i],&zd[i]);
+		con++;
+		printf("%f\t%f\t%f\n",xd[i],yd[i],zd[i]);
+		
+	}
+	fclose(fp);
+    fclose(fs);
+
+
+
+	printf("//////////////////////\n");//linea de separacion 
+
+
+
+
+	int nol2=0,nw2=0,nc2=0; //nol es el numero de lineas que tenemos en el archivo 
+	char ch2;
+
+//contador de lineas del archivo para poder leer solo los datos necesarios del archivo
+    fs2=fopen("random2.txt","r"); 
+    while((ch2=fgetc(fs2))!=EOF)
+    {
+        if(ch2==' ')
+            nw2++;
+        else if(ch2=='\n')
+            nol2++;
+        else
+            nc2++;
+        
+        
+    }
+//leyendo el archivo random2.txt para guardar las coordenadas en arreglos.
+	float xd2[255], yd2[255], zd2[255];
+	int j, con2=0;
+	fp2=fopen("random2.txt","r");
+	for(j=0;j<nol2;j++)
+	{
+		fscanf(fp2,"%f	%f	%f",&xd2[i],&yd2[i],&zd2[i]);
+		con++;
+		printf("%f\t%f\t%f\n",xd2[i],yd2[i],zd2[i]);
+		
+	}
+	fclose(fp2);
+    fclose(fs2);
+
+
+    return 0;
+
+}
