5c3bd4cdc8077deff1e42a0574548635cbb5f75a
saulsierra18 <se.sierraluna@ugto.mx>
2018-12-05 08:51:36 -0600

    Actualizado

--- leer-datos.c
+++ leer-datos.c
@@ -5,20 +5,33 @@
 int main()
 {
 	FILE *fp;
+    FILE *fs;
+    
+    int nol=0,nw=0,nc=0; //nol es el numero de lineas que tenemos en el archivo 
+    char ch;
+    fs=fopen("datos.txt","r");
+    while((ch=fgetc(fs))!=EOF)
+    {
+        if(ch==' ')
+            nw++;
+        else if(ch=='\n')
+            nol++;
+        else
+            nc++;
+        
+        
+    }
 	float xd[255], yd[255], zd[255];
 	int i, con=0;
 	fp=fopen("datos.txt","r");
-	for(i=0;i<10;i++)
+	for(i=0;i<nol;i++)
 	{
 		fscanf(fp,"%f	%f	%f",&xd[i],&yd[i],&zd[i]);
 		con++;
 		printf("%f\t%f\t%f\n",xd[i],yd[i],zd[i]);
 		
 	}
-	fclose(fp);	
-	/*for(i=0;i<10;i++)
-	{
-		printf("%f\t%f\t%f\n",xd[i],yd[i],zd[i]);
-		//printf("%i",con);
-	}	*/
+	fclose(fp);
+    fclose(fs);
+    return 0;
 }
--- proyecto.c
+++ proyecto.c
@@ -6,4 +6,38 @@
 //
 
 #include "proyecto.h"
-hola
+int main ()
+{
+    FILE *fs;
+    FILE *fp;
+    
+    int nol=0,nw=0,nc=0;
+    char ch;
+    fs=fopen("datos.txt","r");
+    while((ch=fgetc(fs))!=EOF)
+    {
+        if(ch==' ')
+            nw++;
+        else if(ch=='\n')
+            nol++;
+        else
+            nc++;
+        
+        
+    }
+   
+    
+    printf("Numero de lineas en el documento=%d \n ",nol);
+    fp=fopen("datos.txt","r");
+    int xo[nol][3],j;
+    printf("%d",xo[nol][3]);
+    //for (j=0; j<nol; j++)
+    //{
+        //(fscanf(fp, "%d", &xo[j][3]);
+        //printf("xo[3][%d] %d\n",j,xo[j][3]);
+       
+    //}
+    fclose(fs);
+    fclose(fp);
+    return 0;
+}
